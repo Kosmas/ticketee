7d555c7fabe5ce5ca3a9209425c959a7f68f809f
Jeremy Tennant <me@j10.io>
2013-07-20 19:01:21 +1000

    Implement creating tickets for a project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130720070046) do
+ActiveRecord::Schema.define(version: 20130720085121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20130720070046) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tickets", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
 
 end
